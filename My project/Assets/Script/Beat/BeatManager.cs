ec0ab20b25172b977aba73db485206a832274a3f
AcacieS <acaciesong@gmail.com>
2025-08-26 15:13:38 -0400

    wait works, now need to fix last note doesn't do its time

--- My project/Assets/Script/Beat/BeatManager.cs
+++ My project/Assets/Script/Beat/BeatManager.cs
@@ -11,7 +11,7 @@ public class BeatManager : MonoBehaviour
     [SerializeField] private Intervals wait;
     [SerializeField] protected int index_interval = 0;
     private bool isFinish = false;
-    private bool isWait = true;
+    //private bool isWait = true;
     private bool waitStarted = false;
     private void Start()
     {
@@ -35,90 +35,73 @@ public class BeatManager : MonoBehaviour
     {
         UpdateOverride();
 
-        if (_intervals[index_interval].getIsFinish())
+        if (_intervals[index_interval].getIsFinish()) //if current is Finish
         {
-            if (index_interval + 1 < _intervals.Length)
-            {
-
-                if (wait != null && isWait)
-                {
-                    // Debug.Log("hey wait");
-                    // // insert wait before advancing
-                    // if (!wait.getIsFinish())
-                    // {
-                    //     float sampledTime = (_audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f));
-                    //     wait.CheckForNewInterval(sampledTime);
-                    //     return; // stop here until wait is done
-                    // }
-                    // else
-                    // {
-                    //     Debug.Log("finish wait");
-                    //     float sampledTime = _audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f);
-                    //     wait.Reset(sampledTime); // reset so it can be reused later
-                    //     index_interval++; // now move to next
-                    //     isWait = false;
-                    // }
-                    
-                    if (!waitStarted)
-                    {
-                        Debug.Log("Wait Started");
-                        float sampledTime = _audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f);
-                        wait.Reset(sampledTime);
-                        waitStarted = true;
-                    }
-                    
-                    // Keep ticking wait
-                    float sampledTime2 = _audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f);
-                    wait.CheckForNewInterval(sampledTime2);
-
-                    if (wait.getIsFinish())
-                    {
-                        // wait done → move to next interval
-                        index_interval++;
-                        isWait = false;
-                        waitStarted = false; // reset for next time
-                        Debug.Log("finish wait, moving to next interval: " + index_interval);
-
-                        // Reset next interval’s steps
-                        float sampledTime3 = _audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f);
-                        for (int i = 0; i < _intervals[index_interval].Size(); i++)
-                            _intervals[index_interval].getIntervals(i).Reset(sampledTime3);
-                    }
-
-                    return; // stop here until wait is done
-                }
-                else
-                {
-                    index_interval++;
-                    Debug.Log("index interval: " + index_interval);
-                    float sampledTime = _audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f);
-                    isWait = true;
-                    for (int i = 0; i < _intervals[index_interval].Size(); i++)
-                    {
-                        _intervals[index_interval].getIntervals(i).Reset(sampledTime);
-                    }
-                }
-                
-            }
-            else
-            {
-
-                Debug.Log(index_interval+": finishhhhhhhhhhhhh");
-                isFinish = true;
-            }
+            NextInterval();
         }
 
         for (int i = 0; i < _intervals[index_interval].Size() && !isFinish; i++)
-            {
-                float sampledTime = (_audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f)); //interval.GetIntervalLength(_bpm)));
-                _intervals[index_interval].getIntervals(i).CheckForNewInterval(sampledTime);
-            }
+        {
+            float sampledTime = (_audioSource.timeSamples / (float)_audioSource.clip.frequency * (_bpm / 60f)); //interval.GetIntervalLength(_bpm)));
+            _intervals[index_interval].getIntervals(i).CheckForNewInterval(sampledTime);
+        }
         
+        
+    }
+    private void NextInterval()
+    {
+        if (index_interval + 1 < _intervals.Length)
+        {
+            Debug.Log("isWait statement: " + _intervals[index_interval].getIsWait());
+            if (wait != null && _intervals[index_interval].getIsWait()) //current wait = true;
+            {
+                Wait();
+            }
+        }
+        else
+        {
+            isFinish = true;
+        }
+        
+    }
+    private void Wait()
+    {
+        //Wait Start the pattern Reset
+        if (!waitStarted)
+        {
+            Debug.Log("Wait Started Reset");
+            float sampledTime = getSampledTime();
+            wait.Reset(sampledTime);
+            waitStarted = true;
+        }
+
+        // Keep ticking wait
+        float sampledTime2 = getSampledTime();
+        wait.CheckForNewInterval(sampledTime2);
+
+        if (wait.getIsFinish())
+        {
+            
+            // wait done → move to next interval
+            index_interval++;
+            waitStarted = false; // reset for next time
+            Debug.Log("finish wait, moving to next interval: " + index_interval);
+            // Reset next interval’s steps
+            float sampledTime3 = getSampledTime();
+            for (int i = 0; i < _intervals[index_interval].Size(); i++)
+                _intervals[index_interval].getIntervals(i).Reset(sampledTime3);
+        }
+
+        return; // stop here until wait is done
+    }
+    private float getSampledTime()
+    {
+        return _audioSource.timeSamples / (float) _audioSource.clip.frequency * (_bpm / 60f);
         
     }
     public virtual void UpdateOverride()
     {
-        
+
 
     }
 
@@ -127,6 +110,7 @@ public class BeatManager : MonoBehaviour
 public class Interval_Pattern
 {
     [SerializeField] private Intervals[] sameTime_interval;
+    private bool isWait = true;
 
     public int Size()
     {
@@ -139,6 +123,16 @@ public class Interval_Pattern
     public bool getIsFinish()
     {
         return sameTime_interval[0].getIsFinish();
+    }
+    
+
+    public bool getIsWait()
+    {
+        return isWait;
+    }
+    public void setIsWait(bool pIsWait)
+    {
+        isWait = pIsWait;
     }
 
 }
@@ -161,10 +155,12 @@ public class Intervals
         _patternIndex = 0;
         _nextBeat = songPositionInBeats;
         _lastInterval = -1;
+        isFinish = false;
     }
 
     public void CheckForNewInterval(float songPositionInBeats)
     {
+        Debug.Log("maybe is Finish" + isFinish);
         if (!isFinish)
         {
             if (_patternIndex < _stepsSO.steps.Length && songPositionInBeats >= _nextBeat)
