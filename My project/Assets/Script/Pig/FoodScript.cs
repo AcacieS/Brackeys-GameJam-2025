4f2135533116c0c1f3d983c025e897e71c92801f
Bazlurman <marcantoine.larochelle05@gmail.com>
2025-08-26 15:27:17 -0400

    Start of the PigGame
    Started working on PigGame (not finished)

---
+++ My project/Assets/Script/Pig/FoodScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FoodScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float beatTempo;
+
+    private void Start() {
+        beatTempo = BeatGame.Instance.getBeat();
+    }
+    private void Update() {
+        transform.position += new Vector3(-1 * beatTempo * Time.deltaTime, 0f, 0f);
+    }
+}
